01ec2286c86b20dbdf29952b3742becb6060df53
Graziela Göedert de Souza <graziela@gmail.com>
2019-05-15 20:49:14 -0300

    Exemplo.

--- Graziela/Modulo3/Curso/src/Aula3/AdivinhaNumero.java
+++ Graziela/Modulo3/Curso/src/Aula3/AdivinhaNumero.java
@@ -7,6 +7,8 @@ import javax.swing.JOptionPane;
 public class AdivinhaNumero {
 	public static void main(String[] args) {
 		Random random = new Random();
+//		double numero =  89.77777;
+//		JOptionPane.showMessageDialog(null, String.format("%6.2f%n", numero));
 		int numero = random.nextInt(100);
 		int numeroUsuario = Integer.parseInt(JOptionPane.showInputDialog("Digite um número!"));
 		if (numero == numeroUsuario) {
