32ff5f97f5378749500e2341110f0803248fc210
behnke <priscillabehnke@gmail.com>
2019-05-21 12:07:38 -0300

    Atualizacao_1

--- Priscilla/Módulo_3/src/Aula_3/Exercicio_1.java
+++ Priscilla/Módulo_3/src/Aula_3/Exercicio_1.java
@@ -1,10 +1,47 @@
 package Aula_3;
 
+import javax.swing.JOptionPane;
+
 public class Exercicio_1 {
 
 	public static void main(String[] args) {
-	
 
+		int valor1 = Integer.parseInt(JOptionPane.showInputDialog("Digite o valor 1: "));
+		int valor2 = Integer.parseInt(JOptionPane.showInputDialog("Digite o valor 2: "));
+		int valor3 = Integer.parseInt(JOptionPane.showInputDialog("Digite o valor 3: "));
+		int maior;
+		int menor;
+		
+		if (valor1 > valor2 && valor1 > valor3) {
+			maior = valor1;
+			
+			if (valor2 < valor3) {
+				menor = valor2;
+				
+			} else {
+				menor = valor3;
+			}
+			
+		} else if (valor2 > valor1 && valor2 > valor3) {
+			maior = valor2;
+			
+			if (valor1 < valor3) {
+				menor = valor1;
+			
+			} else {
+				menor = valor3;
+			}
+		
+		} else {
+			maior = valor3;
+		
+			if (valor1 < valor2) {
+				menor = valor1;
+			
+			} else {
+				menor = valor2;
+			}
+		}
+		JOptionPane.showMessageDialog(null, "O maior valor é: " + maior + "\nO menor valor é: " + menor);
 	}
-
 }
---
+++ Priscilla/Módulo_3/src/Aula_3/Exercicio_4.java
@@ -0,0 +1,22 @@
+package Aula_3;
+
+import javax.swing.JOptionPane;
+
+public class Exercicio_4 {
+
+	public static void main(String[] args) {
+		String letra = JOptionPane.showInputDialog("Digite uma letra:");
+		
+		if (letra == "A") 
+		JOptionPane.showMessageDialog(null, "A letra digitada é uma vogal");
+		break;
+		
+			}
+	
+	else  
+		JOptionPane.showMessageDialog(null, "A letra digitada é uma consoante");
+	break;
+		
+
+	}
+
