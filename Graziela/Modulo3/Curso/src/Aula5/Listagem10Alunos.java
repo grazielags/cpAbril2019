91f7092534d73bfb67361ae13697538f6e6c00c7
Graziela Göedert de Souza <graziela@gmail.com>
2019-05-20 21:10:41 -0300

    Exemplo de aula 4 e 5.

---
+++ Graziela/Modulo3/Curso/src/Aula5/Listagem10Alunos.java
@@ -0,0 +1,11 @@
+package Aula5;
+
+public class Listagem10Alunos {
+	public static void main(String[] args) {
+		int count = 10;
+		while (count >= 1) {
+			System.out.println("Nome " + count + " - Nota " + count);
+			count--;
+		}
+	}
+}
