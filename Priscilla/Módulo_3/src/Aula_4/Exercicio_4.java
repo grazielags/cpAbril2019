c2181442c88a1f338f66c89844d301985a94db70
behnke <priscillabehnke@gmail.com>
2019-05-21 01:59:15 -0300

    Atualização

---
+++ Priscilla/Módulo_3/src/Aula_4/Exercicio_4.java
@@ -0,0 +1,63 @@
+package Aula_4;
+
+import javax.swing.JOptionPane;
+
+public class Exercicio_4 {		
+		public static void main(String[] args) {
+			int numero = Integer.parseInt(JOptionPane.showInputDialog("Informe um número: "));	
+            switch(numero)
+            
+		{
+		case 1:
+			JOptionPane.showMessageDialog(null, "Janeiro - 31 dias!");
+			break;
+		
+		case 2:
+		JOptionPane.showMessageDialog(null, "Fevereiro - 28 dias");
+		break;
+		
+	    case 3:
+		JOptionPane.showMessageDialog(null, "Março - 31 dias");
+		break;
+		
+		case 4:
+			JOptionPane.showMessageDialog(null, "Abril - 30 dias");
+			break;
+			
+		case 5:
+			JOptionPane.showMessageDialog(null, "Maio - 31 dias");
+			break;
+			
+		case 6:
+			JOptionPane.showMessageDialog(null, "Junho - 30 dias");
+			break;
+			
+		case 7:
+			JOptionPane.showMessageDialog(null, "Julho - 31 dias");
+			break;
+			
+		case 8:
+			JOptionPane.showMessageDialog(null, "Agosto - 31 dias");
+			break;
+			
+		case 9:
+			JOptionPane.showMessageDialog(null, "Setembro - 30 dias");
+			break;
+			
+		case 10:
+			JOptionPane.showMessageDialog(null, "Outubro - 31 dias");
+			break;
+			
+		case 11:
+			JOptionPane.showMessageDialog(null, "Novembro - 30 dias");
+			break;
+			
+		case 12:
+			JOptionPane.showMessageDialog(null, "Dezembro - 31 dias");
+			break;		
+			
+		}
+
+	}
+
+}
