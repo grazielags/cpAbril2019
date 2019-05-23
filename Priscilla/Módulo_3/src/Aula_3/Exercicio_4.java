dc06a36cd174b2e654b2f5832f09ed0467519a5a
behnke <priscillabehnke@gmail.com>
2019-05-23 16:46:27 -0300

    Atualizao_Aula_3

--- Priscilla/Módulo_3/src/Aula_3/Exercicio_4.java
+++ Priscilla/Módulo_3/src/Aula_3/Exercicio_4.java
@@ -5,15 +5,18 @@ import javax.swing.JOptionPane;
 public class Exercicio_4 {
 
 	public static void main(String[] args) {
-		JOptionPane.showMessageDialog(null,"*** VOGAL OU CONSOANTES? ***");
-		
-				String letra = JOptionPane.showInputDialog("Entre com a letra:  ");
-				
-					  if ((letra.equalsIgnoreCase("a")) || (letra.equalsIgnoreCase("e"))|| (letra.equalsIgnoreCase("i"))|| (letra.equalsIgnoreCase("o"))|| (letra.equalsIgnoreCase("u"))) {
-						JOptionPane.showMessageDialog(null, "Vogal");
-						} else {
-						JOptionPane.showMessageDialog(null, "Consoante");
-					}
-				}
-			}
+		JOptionPane.showMessageDialog(null, "*** VOGAL OU CONSOANTES ***");
 
+		char letra = JOptionPane.showInputDialog("Entre com a letra:  ").charAt(0);
+
+		if (letra == 'a' || letra == 'A' || letra == 'e' || letra == 'E' || letra == 'i' || letra == 'I' || letra == 'o'
+				|| letra == 'O' || letra == 'u' || letra == 'U') {
+			JOptionPane.showMessageDialog(null, "Vogal");
+		} else if (!(letra >= 65 && letra <= 90) && !(letra >= 97 && letra <= 122)) {
+			JOptionPane.showMessageDialog(null, letra + " não é uma letra válida!");
+
+		} else {
+			JOptionPane.showMessageDialog(null, letra + " é uma consoante!");
+		}
+	}
+}
