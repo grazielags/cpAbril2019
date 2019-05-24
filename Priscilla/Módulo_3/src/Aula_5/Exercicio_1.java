2d29a024f063403e83f2d2688639b25deed9f98b
behnke <priscillabehnke@gmail.com>
2019-05-24 17:22:02 -0300

    Aula_5

---
+++ Priscilla/Módulo_3/src/Aula_5/Exercicio_1.java
@@ -0,0 +1,13 @@
+package Aula_5;
+
+public class Exercicio_1 {
+
+	public static void main(String[] args) {
+	int count = 10;
+	while (count >=1) {
+		System.out.println("Nome" + count + " - Nota " + count);
+		count--;
+	}
+	}
+
+}
